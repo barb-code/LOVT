35e9164c5cd2b16a32858e781e55e06bb1b576f3
barb-code <barbara.souza.pe96@gmail.com>
2021-04-27 13:54:05 -0300

    Validando campo contantohttps://www.devmedia.com.br/html5-validator-validando-formularios-com-html5/28785

--- js/main.js
+++ js/main.js
@@ -40,17 +40,17 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
@@ -61,3 +61,54 @@ function timeFunction() {
 }
 
 timeFunction()
+
+// document.querySelector("#btn-submit-contact").addEventListener("click", (evento) => {
+//   evento.preventDefault(); // cancelando o envio
+
+//   let nomeValue = document.querySelector("#input_nome").value;
+//   let emailValue = document.querySelector("#input_email").value;
+
+//     //validação dos campos
+//     //exibição de mensagens de erro
+//     //envio formulário
+//   console.log(nomeValue);
+//   console.log(emailValue);
+
+// })
+
+
+let formContact = document.querySelector("#form_contact");
+
+
+
+formContact.addEventListener("submit", (evento) => {
+  // interrompo envio formulário
+  evento.preventDefault();
+  let nome = document.querySelector("#input_nome").value;
+
+  let email = document.querySelector("#input_email").value;
+
+  let telefone = document.querySelector("#input_telefone").value;
+
+  let texto = document.querySelector("#mensagem").value;
+  // validar de todos os campos estão preenchidos
+  if (!nome || !email || !telefone || !texto) {
+    alert("Prencha todos os campos!")
+    
+  }
+  // validar se nome tem dois ou mais caracteres
+  if (nome.length < 3) {
+    alert("Seu nome deve ter dois ou mais caracteres")
+    
+  }
+  // validar se telefone tem no mínimo 8 caracteres
+  if (telefone.length < 8) {
+    alert("Seu telefone deve ter no minimo 8 digitos")
+    
+  }
+  // validar se o campo email tem @ (Google - validação email com regex)
+  if (email.indexOf('@') < 0 || email.indexOf('.') < 0) {
+    alert("Campo de email invalido!")
+    
+  }
+})
